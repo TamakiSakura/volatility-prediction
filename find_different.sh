51e64f94c5d23f5dfd0da2e948a4303980cb0ddd
TamakiSakura <tamaki.sakura@hotmail.com>
2016-03-26 17:45:43 -0400

    add read data

--- find_different.sh
+++ find_different.sh
@@ -1,6 +1,6 @@
-${YEAR} = $1
-ls -1 ${YEAR}.tol > filename
-cat ${YEAR}.logvol.+12.txt | awk '{print $1}' > extrafilename
+YEAR=$1
+ls -1 ${YEAR}.tok > filename
+cat ${YEAR}.logvol.+12.txt | awk '{print $2}' > extrafilename
 sed 's/$/.mda/' extrafilename > filename2
 diff filename filename2
 rm filename
--- src/read_data.py
+++ src/read_data.py
@@ -27,20 +27,25 @@ def read_logvol(logvol_filename):
 def construct_doc_term_matrix(tok_folders):
     '''
     Take a list of path to folders where tok.mda file is stored, 
-    and return the numpy NumDoc x NumVocab matrix of doctermatrix
-    Document ordered by CUSIP number
+    and return 
+    1. the numpy NumDoc x NumVocab matrix of doctermatrix
+    Document ordered by Folder order and then by CUSIP number
+    2. number of document in the last folder
     Folders should not end with "/"
     Folders should be ordered by time
     '''
     tm_tdm = textmining.TermDocumentMatrix() 
     document_count = 0
+    document_last_count = 0
     for tok_folder in tok_folders:
         tokfile_list = os.listdir(tok_folder)
         tokfile_list.sort()
+        document_last_count = 0
         for tokfile_name in tokfile_list:
             with open(tok_folder + "/" + tokfile_name) as tokfile:
                 tm_tdm.add_doc(tokfile.readline())
                 document_count += 1
+                document_last_count += 1
     np_tdm = 0 
     row_index = -1
     for row in tm_tdm.rows(cutoff=1):
@@ -49,4 +54,36 @@ def construct_doc_term_matrix(tok_folders):
         else:
             np_tdm[row_index] = row
         row_index += 1
-    return np_tdm
+    return np_tdm, document_last_count
+
+def generate_train_test_set(pred_year, num_prev_year):
+    '''
+    Generate all test/train data, including extra X
+    which is the logvol-12 file
+    '''
+    tok_folder = []
+    X_train_extra = np.zeros(0)
+    Y_train = np.zeros(0)
+    X_test_extra = 0
+    Y_test = 0
+
+    minus_file_name = ".logvol.-12.txt" 
+    plus_file_name = ".logvol.+12.txt"
+
+    for year in range(pred_year - num_prev_year, pred_year + 1):
+        str_year = "../data/" + str(year)
+        tok_folder.append(str_year + ".tok")
+        if year != pred_year:
+            X_train_extra = np.concatenate((X_train_extra, 
+                                            read_logvol(str_year + minus_file_name)))
+            Y_train = np.concatenate((Y_train, 
+                                      read_logvol(str_year + plus_file_name)))
+        else:
+            X_test_extra = read_logvol(str_year + minus_file_name)
+            Y_test = read_logvol(str_year + plus_file_name)
+
+    total_X, test_count = construct_doc_term_matrix(tok_folder)
+    X_train = total_X[:-test_count]
+    X_test = total_X[-test_count:]
+    
+    return X_train_extra, X_train, Y_train, X_test_extra, X_test, Y_test
