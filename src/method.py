03bc7cf9c7ce30003caddfc0d3d1e2de76f9bdfe
guohengw <guo.hengwei1991@gmail.com>
2016-04-02 18:17:39 -0400

    fix the combine method. complete all balseline and lda tests

--- src/method.py
+++ src/method.py
@@ -6,7 +6,9 @@ from sklearn.feature_extraction.text import TfidfTransformer
 
 def topic_from_lda(X_train, X_test, n_topics, n_iter):
     import lda
-    
+    X_train = X_train.astype(int)
+    X_test = X_test.astype(int)
+
     lda_model = lda.LDA(n_topics, n_iter)
     X_reduce_train = lda_model.fit_transform(X_train)
     X_reduce_test = lda_model.transform(X_test)
@@ -18,7 +20,10 @@ def topic_from_LSI(X_train, X_test, n_topics):
     pass
 
 def combine_extra_to_train(extra, train):
-    return scipy.sparse.hstack([np.matrix(extra).T, train])
+    if type(train) == scipy.sparse.csr.csr_matrix:
+        return scipy.sparse.hstack([np.matrix(extra).T, train])
+    else:
+        return np.concatenate((np.matrix(extra).T, train), axis = 1)
 
 def dtm_to_tf(X_train, X_test):
     transformer = TfidfTransformer(norm=None, use_idf = False)
@@ -35,8 +40,8 @@ def dtm_to_tfidf(X_train, X_test):
 def dtm_to_log1p(X_train, X_test):
     return np.log(X_train + 1), np.log(X_test + 1)
 
-def involk_svr(X_total_train, Y_train, X_total_test, Y_test):
-    svr_poly = SVR(kernel='poly', C=1e-5, degree=2)
+def involk_svr(X_total_train, Y_train, X_total_test, Y_test, C=1e-5):
+    svr_poly = SVR(kernel='poly', C=C, degree=2)
     svr_poly.fit(X_total_train, Y_train)
     result = svr_poly.predict(X_total_test)
     return metrics.mean_squared_error(result, Y_test)
--- src/run.py
+++ src/run.py
@@ -8,28 +8,32 @@ import pickle
 os.chdir('/Users/hengweiguo/Documents/repo/volatility-prediction/src')
 
 
-# # X_train and X_test are doc-term matrices
-# # X_train_extra and X_test_extra are v-12 volatilities
-# # Y's are labels
-# X_train_extra, X_train, Y_train, X_test_extra, X_test, Y_test = generate_train_test_set(2006, 2, 0.2)
-#
-# # # store the data
-# train_test_data = TemporaryFile()
-# np.savez('train_test_data', X_train_extra=X_train_extra, X_train=X_train, Y_train=Y_train, X_test_extra=X_test_extra, X_test=X_test, Y_test=Y_test)
-#
+# X_train and X_test are doc-term matrices
+# X_train_extra and X_test_extra are v-12 volatilities
+# Y's are labels
+X_train_extra, X_train, Y_train, X_test_extra, X_test, Y_test = generate_train_test_set(2006, 2, 0.2)
+
+# # store the data
+train_test_data = TemporaryFile()
+np.savez('train_test_data', X_train_extra=X_train_extra, X_train=X_train, Y_train=Y_train, X_test_extra=X_test_extra, X_test=X_test, Y_test=Y_test)
+
 
 # # Getting back the objects:
 npzfile = np.load('train_test_data.npz')
 X_train_extra = npzfile['X_train_extra']
-X_train = npzfile['X_train']
-Y_train = npzfile['Y_train']
+X_train = npzfile['X_train'] # doc term mat
+Y_train = npzfile['Y_train'] # doc term mat
 X_test_extra = npzfile['X_test_extra']
 X_test = npzfile['X_test']
 Y_test = npzfile['Y_test']
 
+n_topics = 20
+n_iter = 200
+X_train_lda, X_test_lda = topic_from_lda(X_train, X_test, n_topics, n_iter)
+
 tf_train, tf_test = dtm_to_tf(X_train, X_test)
 tfidf_train, tfidf_test = dtm_to_tfidf(X_train, X_test)
-# log1p_train, log1p_test = dtm_to_log1p(X_train, X_test)
+log1p_train, log1p_test = dtm_to_log1p(X_train, X_test)
 
 
 # combine the doc-term data and the voliatility data
@@ -39,8 +43,14 @@ X_total_test_tf = combine_extra_to_train(X_test_extra, tf_test)
 X_total_train_tfidf = combine_extra_to_train(X_train_extra, tfidf_train)
 X_total_test_tfidf = combine_extra_to_train(X_test_extra, tfidf_test)
 
-# X_total_train_log1p = combine_extra_to_train(X_train_extra, log1p_train)
-# X_total_test_log1p = combine_extra_to_train(X_test_extra, log1p_test)
+X_total_train_log1p = combine_extra_to_train(X_train_extra, log1p_train)
+X_total_test_log1p = combine_extra_to_train(X_test_extra, log1p_test)
+
+X_total_train_lda= combine_extra_to_train(X_train_extra, X_train_lda)
+X_total_test_lda = combine_extra_to_train(X_test_extra, X_test_lda)
+
+
+#-------------------------- Training and Testing ----------------------------
 
 # train and test with the baseline: V-12
 mse_V_minus_12 = baseline(X_test_extra, Y_test)
@@ -51,7 +61,13 @@ mse_V_tf_plus = involk_svr(X_total_train_tf, Y_train, X_total_test_tf, Y_test)
 # train and test with TFIDF+
 mse_V_tfidf_plus = involk_svr(X_total_train_tfidf, Y_train, X_total_test_tfidf, Y_test)
 
+# train and test with LDA
+mse_V_lda = involk_svr(X_train_lda, Y_train, X_test_lda, Y_test)
+
+# train and test with LDA+ volatility
+mse_V_lda_plus = involk_svr(X_total_train_lda, Y_train, X_total_test_lda, Y_test)
+
 # train and test with log1p+
-# mse_V_log1p_plus = involk_svr(X_total_train_log1p, Y_train, X_total_test_log1p, Y_test)
+mse_V_log1p_plus = involk_svr(X_total_train_log1p, Y_train, X_total_test_log1p, Y_test)
 
 print(mse_V_minus_12)
