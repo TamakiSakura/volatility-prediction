b4a774d7f933eb756d170727156f057a705c3bf6
TamakiSakura <tamaki.sakura@hotmail.com>
2016-04-02 15:06:12 -0400

    add tfidf stuff

--- src/method.py
+++ src/method.py
@@ -1,6 +1,7 @@
 import numpy as np
 from sklearn.svm import SVR
 from sklearn import metrics
+from sklearn.feature_extraction.text import TfidfTransformer
 
 def topic_from_lda(X_train, X_test, n_topics, n_iter):
     import lda
@@ -15,7 +16,22 @@ def topic_from_LSI(X_train, X_test, n_topics):
     pass
 
 def combine_extra_to_train(extra, train):
-    return numpy.concatenate((extra, train), axis = 1)
+    return numpy.concatenate((np.matrix(extra).T, train), axis = 1)
+
+def dtm_to_tf(X_train, Y_train):
+    transformer = TfidfTransformer(norm=None, use_idf = False)
+    tfidf_X = transformer.fit_transform(X_train)
+    tfidf_Y = transformer.transform(Y_train)
+    return tfidf_X, tfidf_Y
+
+def dtm_to_tfidf(X_train, Y_train):
+    transformer = TfidfTransformer(norm=None, use_idf = True)
+    tfidf_X = transformer.fit_transform(X_train)
+    tfidf_Y = transformer.transform(Y_train)
+    return tfidf_X, tfidf_Y
+
+def dtm_to_log1p(X_train, Y_train):
+    return np.log(X_train + 1), np.log(Y_train + 1)
 
 def involk_svr(X_total_train, Y_train, X_total_test, Y_test):
     svr_poly = SVR(kernel='poly', C=1e3, degree=2)
