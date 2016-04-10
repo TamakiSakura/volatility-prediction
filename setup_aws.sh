1a7dd2a66c1d1e89e4b15a1c212416485077bb2b
Tamaki Sakura <tamaki.sakura@hotmail.com>
2016-04-10 14:39:33 -0400

    Change a lot

--- setup_aws.sh
+++ setup_aws.sh
@@ -1,14 +1,35 @@
+set -e
+
+function cleanup {
+  echo "Error somewhere see above for more detail"
+  set +e
+}
+trap cleanup EXIT
+
 sudo yum groupinstall "Development Tools"
 sudo yum install tmux atlas-sse3-devel lapack-devel
 
 # Provide from https://gist.github.com/dacamo76/4780765
 wget https://pypi.python.org/packages/source/v/virtualenv/virtualenv-1.11.6.tar.gz
 tar xzf virtualenv-1.11.6.tar.gz 
+rm virtualenv-1.11.6.tar.gz 
 python27 virtualenv-1.11.6/virtualenv.py sk-learn
 . sk-learn/bin/activate
 
-sudo pip install cython scipy numpy
-sudo pip install scikit-learn pymc
-sudo pip install lda
-sudo pip install textmining
-sudo pip install -U pip
+pip install -U pip
+
+# This part is only for if you are using a instance with memory less than 2GB
+# Creating a swap file of 2GB for scipy installation
+# dd if=/dev/zero of=/swapfile1 bs=1024 count=2097152
+# chown root:root /swapfile1
+# chmod 0600 /swapfile1
+# mkswap /swapfile1
+# swapon /swapfile1
+
+pip install cython numpy
+pip install scipy
+pip install scikit-learn pymc
+pip install lda textmining
+
+# swapoff /swapfile1
+# swapon -s
